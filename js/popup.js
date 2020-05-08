bfba99f1837f39409c46d52bb241a8e0ddb54c05
nosna <ansonhuang1216@gmail.com>
2020-05-08 00:16:18 -0400

    init

--- js/popup.js
+++ js/popup.js
@@ -17,23 +17,23 @@ var COMMAND_START = "command_start";
 var state = true;
 
 // Checkout Path
-var amazonCheckoutPath = '/gp/buy/shipoptionselect/handlers';
-var amazonPrimeCheckoutPath = '/checkout/enter-checkout';
-var instaCheckoutPath = '/store/checkout_v3';
-var amazonFreshCheckoutPath = '/afx/slotselection';
+// var amazonCheckoutPath = '/gp/buy/shipoptionselect/handlers';
+// var amazonPrimeCheckoutPath = '/checkout/enter-checkout';
+// var instaCheckoutPath = '/store/checkout_v3';
+// var amazonFreshCheckoutPath = '/afx/slotselection';
 
 window.onload = function () {
   // Detect if there any checkout pages on the screen
-  chrome.tabs.query({}, function (tabs) {
-    for (var i = 0; i < tabs.length; ++i) {
-      tab = tabs[i];
-      if (tab.url.match(amazonCheckoutPath) || tab.url.match(amazonPrimeCheckoutPath) ||
-        tab.url.match(instaCheckoutPath) || tab.url.match(amazonFreshCheckoutPath)) {
-        return;
-      }
-    }
-    document.getElementById(ALERT_TEXT_ID).innerHTML = '<p style="color:red">Error: can\'t find schedule pages</p>';
-  });
+  // chrome.tabs.query({}, function (tabs) {
+  //   for (var i = 0; i < tabs.length; ++i) {
+  //     tab = tabs[i];
+  //     if (tab.url.match(amazonCheckoutPath) || tab.url.match(amazonPrimeCheckoutPath) ||
+  //       tab.url.match(instaCheckoutPath) || tab.url.match(amazonFreshCheckoutPath)) {
+  //       return;
+  //     }
+  //   }
+  //   document.getElementById(ALERT_TEXT_ID).innerHTML = '<p style="color:red">Error: can\'t find schedule pages</p>';
+  // });
 
   // Init the extension when popup  
   chrome.storage.sync.get({
@@ -53,22 +53,22 @@ window.onload = function () {
   });
 
   // Counter init
-  chrome.storage.sync.get(['count_init'], function (result) {
-    if (!result.count_init) {
-      chrome.storage.sync.set({
-        'succ_count': 0
-      });
-      chrome.storage.sync.set({
-        'count_init': true
-      });
-    } else {
-      // Display notification counts on the panel
-      chrome.storage.sync.get(['succ_count'], function (result) {
-        cur_count = result.succ_count;
-        document.getElementById(COUNTER_TEXT_ID).innerHTML = cur_count;
-      });
-    }
-  });
+  // chrome.storage.sync.get(['count_init'], function (result) {
+  //   if (!result.count_init) {
+  //     chrome.storage.sync.set({
+  //       'succ_count': 0
+  //     });
+  //     chrome.storage.sync.set({
+  //       'count_init': true
+  //     });
+  //   } else {
+  //     // Display notification counts on the panel
+  //     chrome.storage.sync.get(['succ_count'], function (result) {
+  //       cur_count = result.succ_count;
+  //       document.getElementById(COUNTER_TEXT_ID).innerHTML = cur_count;
+  //     });
+  //   }
+  // });
 
   // Start button onclick  
   document.getElementById(START_BUTTON_ID).onclick = function () {
@@ -95,7 +95,7 @@ window.onload = function () {
         });
         break;
     }
-  }
+  };
 
   // Set stop button state
   function setButtonStop() {
